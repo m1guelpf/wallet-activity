c79c153e7195ee68f33388ceb984d3a2737d1246
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-02-25 10:03:52 +0100

    wip

--- src/pages/api/_seed-db.ts
+++ src/pages/api/_seed-db.ts
@@ -1,11 +1,12 @@
 import handler from '@/lib/api-handler'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { Network } from '@/types/utils'
 import { seedTintin } from '@/lib/seeders'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export default handler.get(async ({ query: { network = 'mainnet' } }: NextApiRequest, res: NextApiResponse) => {
 	if (process.env.NODE_ENV !== 'development') return res.status(404).send('Not Found')
 
-	await seedTintin({ network: network as string })
+	await seedTintin({ network: network as Network })
 
 	res.status(200).send(0)
 })
