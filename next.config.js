78eb641855f477c6693b1da7ce85db52847987e0
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-03-03 17:46:13 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const { withSentryConfig } = require('@sentry/nextjs')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	sentry: {
-		hideSourceMaps: true,
-	},
 }
 
 module.exports = withSentryConfig(nextConfig, { silent: true })
